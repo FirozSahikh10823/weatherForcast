0e019009e5ab96dc9e34d8c69d74c5f8b318c98f
shaikhmohds <shaikhmohds@BSMUML-027320>
2024-04-22 19:09:46 +0530

    New controller added

---
+++ weatherForcast/Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace weatherForcast
+{
+    public class HomeController : Controller
+    {
+        // GET: HomeController
+        public int Index()
+        {
+            return 10;
+        }
+
+        
+    }
+}
